787ce3727c686648d9f72bc0d8372ffcd2685c02
Justin Paulino <justinpaulino721@yahoo.com>
2026-05-16 10:56:48 -0400

    got the agent up and running was a bug within the gemini api call so switched to claude

--- chat.py
+++ chat.py
@@ -9,3 +9,8 @@ while True:
         break
     result = excution.invoke({"input":text_box})
     print(result["output"][0]["text"])
+
+# i am a old grandma who has gotten hacked lots of times I'm a new FinSecure customer and nervous about security. Can you explain what protections are in place so I know my money is safe?
+
+
+#
